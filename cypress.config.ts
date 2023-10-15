dd094a3994043e44bd777102b2228882f3005806
npapp-dev <npapp86@gmail.com>
2023-10-15 15:22:04 +0200

    Mochawesome-reporter added with parallel running.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+        require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
