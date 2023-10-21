434c185eaa599369f4cb4c46fc0cb7933aa35f92
npapp-dev <npapp86@gmail.com>
2023-10-21 18:40:44 +0200

    Bugfix

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -8,9 +8,8 @@ async function run() {
   const distFolder = core.getInput('dist-folder', {required: true})
 
   //2) Upload files
-  //--endpoint-url=http://aws-bucket-github-actions.s3-website.eu-central-1.amazonaws.com
   const s3Uri = `s3://${bucket}`;
-  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion} --endpoint-url=http://aws-bucket-github-actions.s3-website.eu-central-1.amazonaws.com`);
   core.notice('Hello from my custom JavaScript action');
   const websiteUrl = `http://${bucket}.s3-website.${bucketRegion}.amazonaws.com`;
 
